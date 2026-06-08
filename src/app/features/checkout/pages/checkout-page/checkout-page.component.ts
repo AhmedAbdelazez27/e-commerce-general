f2f7b2f0abf4b65341164ac9e3a739b08737d300
AhmedAbdelazez27 <ahmed.abdelazeez@jrm.com.eg>
2026-06-08 11:11:07 +0300

    add design mo

---
+++ src/app/features/checkout/pages/checkout-page/checkout-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-checkout-page',
+  imports: [TranslateModule],
+  templateUrl: './checkout-page.component.html',
+})
+export class CheckoutPageComponent {}
